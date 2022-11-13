c04fa8205a1ea5118a9932fe685dd59872a32f5f
Secret <97087168+secret-pommes@users.noreply.github.com>
2022-11-13 19:33:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+
+const port = 80
+
+app.listen(port, ()=>{
+    console.log("Started listening on port:", port)
+})
+
+// main site
+app.get("/", async (req, res)=>{
+    res.sendFile(path.join(__dirname, "/web/index.html"))
+})
+
+app.get("/status", async (req, res)=>{
+    res.sendFile(path.join(__dirname, "/web/subsites/status.html"))
+})
+
+// internal status
+app.get("/internalstatus", async (req, res)=>{
+    res.sendStatus(200)
+})
