eb409ce84d3ce94fa5217ce92b4b68baf9269f3f
Secret <97087168+secret-pommes@users.noreply.github.com>
2022-11-13 19:34:29 +0100

    fixed some port shit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path")
 
-const port = 80
+const port = process.env.PORT || 80
 
 app.listen(port, ()=>{
     console.log("Started listening on port:", port)
