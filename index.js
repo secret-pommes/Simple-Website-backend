9688a3c2b2160b68ea1ac3b682b331f5db7c2a71
Secret <97087168+secret-pommes@users.noreply.github.com>
2022-11-13 19:36:38 +0100

    added some examples

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path")
 
+// port
 const port = process.env.PORT || 80
 
+// server listen part 
 app.listen(port, ()=>{
     console.log("Started listening on port:", port)
 })
@@ -13,11 +15,19 @@ app.get("/", async (req, res)=>{
     res.sendFile(path.join(__dirname, "/web/index.html"))
 })
 
+// a subsite
 app.get("/status", async (req, res)=>{
     res.sendFile(path.join(__dirname, "/web/subsites/status.html"))
 })
 
-// internal status
+// again a subsite
 app.get("/internalstatus", async (req, res)=>{
     res.sendStatus(200)
+})
+
+
+app.get("*", (req, res)=>{
+    res.json({
+    "message":"Error 404"
+    })
 })
